8d559fccb484cbc049986ef61efb47739931969f
MatheusMelo <mtheusmelo.s@gmail.com>
2024-08-02 08:17:57 -0300

    feat: create Player header

---
+++ src/Player.hpp
@@ -0,0 +1,31 @@
+#ifndef PLAYER_HPP
+#define PLAYER_HPP
+
+#include <string>
+#include <fstream>
+#include <map>
+
+class Player {
+  private:
+    std::string _name;
+    std::string _nickName;
+    std::map<std::string, int> _victories;
+    std::map<std::string, int> _defeats;
+
+  public:
+    Player() = default;
+    Player(std::string nickName, std::string name);
+    ~Player();
+
+    void showStatistics() const;
+    void readPlayers(std::ifstream& in);
+    void writePlayers(std::ofstream& out) const;
+
+    std::string getName() const;
+    std::string getNickName() const;
+
+    void setVictories(std::string gameName);
+    void setDefeats(std::string gameName);
+};
+
+#endif
