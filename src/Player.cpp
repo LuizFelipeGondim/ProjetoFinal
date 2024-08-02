071082e559611ddbebde62290a1eac282b634254
MatheusMelo <mtheusmelo.s@gmail.com>
2024-08-02 08:19:08 -0300

    feat: create Player class

---
+++ src/Player.cpp
@@ -0,0 +1,48 @@
+#include "Player.hpp"
+#include <iostream>
+#include <string>
+
+Player::Player(std::string nickName, std::string name) : 
+  _nickName(nickName), _name(name){
+    _victories["reversi"] = 0;
+    _victories["connectFour"] = 0;
+    _defeats["reversi"] = 0;
+    _defeats["connectFour"] = 0;
+  }
+
+Player::~Player(){}
+
+void Player::showStatistics() const{
+  std::cout << _nickName << " " << _name << std::endl;
+  std::cout << "REVERSI - VITÓRIAS: "<< _victories.at("reversi") << " DERROTAS: " << _defeats.at("reversi") << std::endl;
+  std::cout << "LIG4 - VITÓRIAS: "<< _victories.at("connectFour") << " DERROTAS: " << _defeats.at("connectFour") << std::endl;
+  std::cout << std::endl;
+}
+
+void Player::readPlayers(std::ifstream& in){
+  in >> _nickName >> _name;
+  in >> _victories["reversi"] >> _defeats["reversi"];
+  in >> _victories["connectFour"] >> _defeats["connectFour"];
+}
+
+void Player::writePlayers(std::ofstream& out) const{
+  out << _nickName << " " << _name << " ";
+  out << _victories.at("reversi") << " " << _defeats.at("reversi") << " ";
+  out << _victories.at("connectFour") << " " << _defeats.at("connectFour") << std::endl;
+}
+
+std::string Player::getName() const{
+  return _name;
+}
+
+std::string Player::getNickName() const{
+  return _nickName;
+}
+
+void Player::setVictories(std::string gameName){
+  _victories[gameName]++;
+}
+
+void Player::setDefeats(std::string gameName){
+  _defeats[gameName]++;
+}
