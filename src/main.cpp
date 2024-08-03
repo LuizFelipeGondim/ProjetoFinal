42f773b27fd875d40723beb5101ccad3c5ae6ff0
LuizFelipeGondim <luizfelipegondimaraujo@gmail.com>
2024-08-03 01:51:37 -0300

    feat: update main board structure

--- src/main.cpp
+++ src/main.cpp
@@ -1,122 +1,74 @@
+#include "../include/GameBoard.hpp"
+
 #include <iostream>
 #include <string>
 #include <vector>
 
+const std::string GameBoard::FILENAME = "players.txt";
+
 int main(){
-    //const GameBoard gameBoard;
-    std::string option;
+  GameBoard gameBoard;
+  std::string option;
 
-    while((std::cin >> option) && option != "FS"){
+  while((std::cin >> option) && option != "FS"){
 
-        if (option == "CJ"){
-            bool playerExists;
-            std::string nickname, name;
+    if (option == "CJ"){
+      bool playerExists;
+      std::string nickName, name;
 
-            std::cin >> nickname >> name;
+      std::cin >> nickName >> name;
 
-            /*
-            playerExists = gameBoard.searchPlayer(nickname);
+      playerExists = gameBoard.searchPlayer(nickName);
 
-            if(!playerExists){
-                gameBoard.registerPlayer(nickname, name);
-                std::cout << "Jogador " << nickname << " cadastrado com sucesso!" << std::endl;
-            }else{
-                std::cout << "ERRO: Player existente!" << std::endl;
-            } 
-            */
+      if(!playerExists){
+        gameBoard.registerPlayer(nickName, name);
+        std::cout << "Jogador " << nickName << " cadastrado com sucesso!" << std::endl;
+      }else{
+        std::cout << "ERRO: Jogador existente!" << std::endl;
+      }
 
-            continue;
-        }
+      std::cout << std::endl;
 
-        if (option == "RJ"){
-            bool playerExists;
-            std::string nickname;
-            std::cin >> nickname;
+    } else if (option == "RJ"){
+      bool playerExists;
+      std::string nickName;
+      std::cin >> nickName;
 
-            /*
-            playerExists = gameBoard.searchPlayer(nickname);
-            
-            if(playerExists){
-                gameBoard.removePlayer(nickname);
-                std::cout << "Jogador " << nickname << " removido com sucesso!" << std::endl;
-            }else{
-                std::cout << "ERRO: Player inexistente!" << std::endl;
-            } 
-            */
-            continue;
-        }
+      playerExists = gameBoard.searchPlayer(nickName);
+      
+      if(playerExists){
+        gameBoard.removePlayer(nickName);
+        std::cout << "Jogador " << nickName << " removido com sucesso!" << std::endl;
+      }else{
+        std::cout << "ERRO: Jogador inexistente!" << std::endl;
+      } 
+      
+      std::cout << std::endl;
 
-        if (option == "LJ"){
-            char orderType;
-            std::cin >> orderType;
+    } else if (option == "LJ"){
+      std::string orderType;
 
-            /*
-            while(orderType != 'A' && orderType != 'N'){
-                std::cout << "Informe um método ordenação válido: [A|N]" << std::endl;
-                std::cin >> orderType; 
-            }
+      std::cout << std::endl;
+      std::cout << "Informe um método ordenação: [A|N]" << std::endl;
+      std::cin >> orderType;
 
-            gameBoard.listStatistics();
-            */
+      while(orderType != "A" && orderType != "N"){
+        std::cout << std::endl;
+        std::cout << "Informe um método de ordenação válido: [A|N]" << std::endl;
+        std::cin >> orderType; 
+      }
 
-            continue;
-        }
+      gameBoard.listStatistics(orderType);
 
-        if (option == "EP") {
+    } else if (option == "EP"){
 
-            continue;
-        }
-    }
-
-    return 0;
-}
-
-/*
-void readPlayersFromFile(const std::string filename, std::vector<Player>& players){
-
-    std::ifstream infile(filename);
-    if (!infile) {
-        std::cerr << "Erro leitura do arquivo: " << filename << std::endl;
-        return;
-    }
-
-    while (!infile.eof()) {
-        Player player;
-        player.readPlayers(infile);
-
-        if (infile) { 
-            players.push_back(player);
-        }
-    }
-
-    infile.close();
-}
-
-void writePlayersToFile(const std::string filename, const std::vector<Player>& players){
-
-    std::ofstream outfile(filename);
-    if (!outfile) {
-        std::cerr << "Erro leitura do arquivo: " << filename << std::endl;
-        return;
-    }
-
-    for (const auto& player : players) {
-        player.writePlayers(outfile);
-    }
-
-    outfile.close();
-}
-
-void removePlayer(const std::string nickName, std::vector<Player>& players){
-    auto it = std::remove_if(players.begin(), players.end(), [&nickName](const Player& player) {
-        return player.getNickName() == nickName;
-    });
-
-    if (it != players.end()) {
-        players.erase(it, players.end());
-        std::cout << nickName << " removido com sucesso!" << std::endl;
     } else {
-        std::cout << nickName << " não foi encontrado." << std::endl;
+      std::cout << "Opção inválida!" << std::endl;
+      std::cout << std::endl;
     }
+  }
+
+  gameBoard.writePlayersToFile();
+
+  return 0;
 }
-*/
